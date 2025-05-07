6eeacc827f50080e7b5324c590f22952a9588b71
Valerio Mazza <vallo1987@gmail.com>
2025-05-07 10:27:17 +0200

    feat: init vue-query client

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query'
 
 import App from './App.vue'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import router from './router'
 
 const app = createApp(App)
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      refetchOnWindowFocus: true,
+      refetchOnReconnect: true,
+      retry: 1,
+      retryDelay: 1000,
+    },
+  },
+})
+
 app.use(createPinia())
+app.use(VueQueryPlugin, { queryClient })
 app.use(router)
 
 app.mount('#app')
